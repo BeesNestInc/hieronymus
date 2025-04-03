39d154971aef3e35c030200326b6799651733a67
Masami Ogoshi <ogochan@wasp.co.jp>
2025-04-03 13:05:37 +0900

    prepare

---
+++ front/svelte/customer/customer-home.svelte
@@ -0,0 +1,23 @@
+<div class="page-title d-flex justify-content-between">
+  <h1>設定</h1>
+</div> 
+<div class="col-6" style="padding:10px;">
+  <TableMaintenance
+    title={'取引先種別'}
+    endpoint={'/api/customer/kinds'}
+    bind:minimize={customerMinimize}
+    columns={[
+      { type: "id", name: 'id'},
+      { type: "order", name: 'displayOrder'},
+      { type: "text", name: 'name', title: "種別名", align: "left"},
+      { type: "checkbox", name: 'isClient', title: "顧客", width: "50px"}
+  ]}></TableMaintenance>
+</div>
+
+<script>
+import TableMaintenance from '../components/table-maintenance.svelte';
+
+export let status;
+
+let customerMinimize = false;
+</script>
