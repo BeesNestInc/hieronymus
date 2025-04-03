d4dff1e6d6b76a31b72bd7edcf9af839534ff48e
Masami Ogoshi <ogochan@wasp.co.jp>
2025-04-03 12:58:38 +0900

    prepare

---
+++ front/javascripts/form.js
@@ -0,0 +1,7 @@
+import Index from '../svelte/forms/index.svelte';
+
+const index = new Index({
+  target: document.getElementById('app')
+});
+
+export default index;
---
+++ front/javascripts/routes.js
@@ -0,0 +1,8 @@
+import Test from '../svelte/forms/estimate/Test.svelte';
+
+export default [
+  {
+    path: '/forms/transaction/estimate/:id',
+    component: Test
+  }
+];
