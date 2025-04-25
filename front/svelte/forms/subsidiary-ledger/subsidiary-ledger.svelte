67e5e0115b62b48b48f266953d12339910c39491
Masami Ogoshi <ogochan@wasp.co.jp>
2025-04-26 01:50:06 +0900

    form recover

---
+++ front/svelte/forms/subsidiary-ledger/subsidiary-ledger.svelte
@@ -0,0 +1,22 @@
+<FrontCover
+	title="補助元帳"
+  {fy}
+  {company}
+  ></FrontCover>
+{#each ledgerPages as ledgerPage}
+<LedgerPage
+  {fy}
+  {company}
+	title="補助元帳"
+  {ledgerPage}
+  ></LedgerPage>
+{/each}
+<script>
+import FrontCover from '../components/front-cover.svelte';
+import LedgerPage from '../components/ledger-page.svelte';
+
+export let fy;
+export let company;
+export let ledgerPages;
+
+</script>
